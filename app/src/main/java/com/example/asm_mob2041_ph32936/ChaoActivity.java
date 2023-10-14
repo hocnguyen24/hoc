04de057025ab556b7119f41289563ded5631c4d2
unknown <hocncph36330@fpt.edu.vn>
2023-10-14 16:03:13 +0700

    Nguyen Hoc

---
+++ app/src/main/java/com/example/asm_mob2041_ph32936/ChaoActivity.java
@@ -0,0 +1,26 @@
+package com.example.asm_mob2041_ph32936;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class ChaoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chao);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(ChaoActivity.this, DangNhapActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.slide_out_top,R.anim.slide_out_down);
+                finish();
+            }
+        },3000);
+    }
+}
---
+++ app/src/main/java/com/example/asm_mob2041_ph32936/Model/ThanhVien.java
@@ -0,0 +1,56 @@
+package com.example.asm_mob2041_ph32936.Model;
+
+public class ThanhVien {
+    private int maTV;
+    private String hoTen;
+    private int namSinh;
+
+    private String cccd;
+
+    public ThanhVien() {}
+
+    public ThanhVien(int maTV, String hoTen, int namSinh) {
+        this.maTV = maTV;
+        this.hoTen = hoTen;
+        this.namSinh = namSinh;
+    }
+
+    public ThanhVien(int maTV, String hoTen, int namSinh, String cccd) {
+        this.maTV = maTV;
+        this.hoTen = hoTen;
+        this.namSinh = namSinh;
+        this.cccd = cccd;
+    }
+
+    public int getMaTV() {
+        return maTV;
+    }
+
+    public void setMaTV(int maTV) {
+        this.maTV = maTV;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public int getNamSinh() {
+        return namSinh;
+    }
+
+    public void setNamSinh(int namSinh) {
+        this.namSinh = namSinh;
+    }
+
+    public String getCccd() {
+        return cccd;
+    }
+
+    public void setCccd(String cccd) {
+        this.cccd = cccd;
+    }
+}
